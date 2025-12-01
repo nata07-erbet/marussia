107b5ccb7da364301fd1f6f5032f1ce72035ab66
Nata <erbet@mail.ru>
2025-12-01 21:05:38 +0300

    init repo

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './components/app/app';
+
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
