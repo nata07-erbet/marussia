910fa40f026cb7c8082a0c5c6b37a190204b662b
Nata <erbet@mail.ru>
2025-12-08 18:49:27 +0300

    add types

---
+++ src/types/types.ts
@@ -0,0 +1,69 @@
+type IFavorites = string;
+    
+type IUser = {
+    name: string;        
+    surname: string;     
+    email: string;       
+    favorites: IFavorites[]; 
+};
+
+type IAuthInfo = {
+    email: string;
+    password: string;
+};
+
+type ISuccessfulResult = {
+    result: boolean;
+};
+
+type IError = {
+    error: string;
+};
+
+type IRegisterData = {
+    email: string;
+    password: string;
+    name?: string;
+    surname?: string;
+};
+
+type IMovie = {
+    id: number;
+    title: string;
+    originalTitle: string;
+    language: string;
+    releaseYear: number;
+    releaseDate: string;
+    genres: string[]; 
+    plot: string;
+    runtime: number; 
+    budget: string;
+    revenue: string;
+    homepage: string;
+    status: string;
+    posterUrl: string;
+    backdropUrl: string;
+    trailerUrl: string;
+    trailerYoutubeId: string;
+    tmdbRating: number; 
+    searchL: string;
+    keywords: string[];
+    countriesOfOrigin: string[];
+    languages: string[];
+    cast: string[];
+    director: string;
+    production: string;
+    awardsSummary: string;
+  };
+  
+
+  type IApiResponse = {
+    code: number;
+    type: string;
+    message: string;
+  };
+  
+  type IFavoritesBody = {
+    id: string;
+  };
+
