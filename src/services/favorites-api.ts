4865aa65e4b988e79e27d07c5e35d4ee628cb19a
Nata <erbet@mail.ru>
2025-12-11 17:09:20 +0300

    add favor

---
+++ src/services/favorites-api.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { BASE_URL, ReqRoutes } from '../const/const';
+import { IAuthResult, IRegisterData, IAuthInfo, IProfile, IMovie } from '../types/types';
+
+
+const favoritesApi = createApi({
+    reducerPath: 'favoritesApi',
+    baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+    endpoints: (builder) => ({
+        fetchFavorites: builder.query<IMovie[], void>({
+            query: () => ReqRoutes.FAVORITES
+        }),
+        addFavorites: builder.mutation<IProfile,IMovie>({
+            query: (film) => ({
+                url: ReqRoutes.FAVORITES,
+                method: 'POST',
+                body: film
+            })
+        }),
+    })
+});
