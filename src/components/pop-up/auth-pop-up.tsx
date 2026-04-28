609d7de2b76d690bcd1aab4bc4b4b0963475ad59
Nata <erbet@mail.ru>
2026-04-28 20:37:17 +0300

    finish auth errors

--- src/components/pop-up/auth-pop-up.tsx
+++ src/components/pop-up/auth-pop-up.tsx
@@ -3,12 +3,11 @@ import axios from 'axios';
 import { StatusCodes } from 'http-status-codes';
 import { PopUpSample, PopUpSampleProps } from './pop-up-sample';
 import styled from 'styled-components';
-import { ButtonFirst } from '../button-first';
 import { ButtonSecond } from '../button-second';
 import { useForm } from 'react-hook-form';
 
 
-const Modal = styled.div`
+const Modal = styled.div<{ $isError?: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -17,7 +16,7 @@ const Modal = styled.div`
   border-radius: 24px;
   padding: 64px 40px;
   width: 420px;
-  height: 455px;
+  height: ${(props) => (props.$isError ? '500px' : '455px')};
   background-color: #fff;
 `;
 
@@ -32,17 +31,17 @@ const ImgPopUp = styled.img`
   margin-bottom: 40px;
 `;
 
-const Label = styled.label`
+const Label = styled.label<{ $isError?: boolean }>`
   display: flex;
   gap: 12px;
-  border: 1px solid rgba(0, 0, 0, 0.4);
+  border: ${(props) => (props.$isError ? '1px solid #ff7575' : '1px solid rgba(0, 0, 0, 0.4)')};
   border-radius: 8px;
   padding: 16px;
   width: 340px;
   height: 56px;
 
   &:first-of-type {
-    margin-bottom: 16px;
+    margin-bottom: ${(props) => (props.$isError ? '' : '16px')};
   }
 `;
 
@@ -54,6 +53,10 @@ const Input = styled.input`
   font-size: 18px;
   line-height: 133%;
   color: rgba(0, 0, 0, 0.4);
+`;
+
+const Svg = styled.svg<{ $isError: boolean }>`
+  fill: ${(props) => (props.$isError ? '#ff7575' : 'rgba(0, 0, 0, 0.4)')}
 `;
 
 const ButtonEl = styled.button`
@@ -90,8 +93,6 @@ function AuthPopUp({ onClickRegistration, onSubmit, ...props }: AuthPopUpProps) 
   const {
     register,
     handleSubmit,
-    watch,
-    setValue,
     formState: { errors }
   } = useForm<FormInputs>();
 
@@ -110,27 +111,26 @@ function AuthPopUp({ onClickRegistration, onSubmit, ...props }: AuthPopUpProps) 
   return (
     <PopUpSample {...props}>
       <WrapperAuth>
-        <Modal>
+        <Modal $isError={!!errors.email || !!errors.password}>
           <ImgPopUp
             src='/assets/marussia_popup.png'
             width='156px'
             height='35px'
           ></ImgPopUp>
           <form onSubmit={handleSubmit(onSubmitData)}>
-            <Label>
-              <svg
+            <Label $isError={!!errors.email}>
+              <Svg 
+                $isError={!!errors.email}
                 width='24'
                 height='24'
                 viewBox='0 0 24 24'
-                fill='none'
                 xmlns='http://www.w3.org/2000/svg'
               >
                 <path
                   d='M21 3C21.5523 3 22 3.44772 22 4V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V19H20V7.3L12 14.5L2 5.5V4C2 3.44772 2.44772 3 3 3H21ZM8 15V17H0V15H8ZM5 10V12H0V10H5ZM19.5659 5H4.43414L12 11.8093L19.5659 5Z'
-                  fill='black'
                   fillOpacity='0.4'
                 />
-              </svg>
+              </Svg>
               <Input
                 {...register('email', {
                   required: {
@@ -148,23 +148,29 @@ function AuthPopUp({ onClickRegistration, onSubmit, ...props }: AuthPopUpProps) 
               ></Input>
             </Label>
             {errors.email && (<PError>{errors.email.message}</PError>)}
-            <Label>
-              <svg
+            <Label $isError={!!errors.password}>
+              <Svg
+                $isError={!!errors.password}
                 width='24'
                 height='24'
                 viewBox='0 0 24 24'
-                fill='none'
                 xmlns='http://www.w3.org/2000/svg'
               >
                 <path
                   d='M12.917 13C12.441 15.8377 9.973 18 7 18C3.68629 18 1 15.3137 1 12C1 8.68629 3.68629 6 7 6C9.973 6 12.441 8.16229 12.917 11H23V13H21V17H19V13H17V17H15V13H12.917ZM7 16C9.20914 16 11 14.2091 11 12C11 9.79086 9.20914 8 7 8C4.79086 8 3 9.79086 3 12C3 14.2091 4.79086 16 7 16Z'
-                  fill='black'
                   fillOpacity='0.4'
                 />
-              </svg>
+              </Svg>
               <Input
                 {...register('password', {
-                  required: true,
+                  required: {
+                    value: true,
+                    message: 'Введите пароль'
+                  },
+                  pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d).{6,12}$/,
+                    message: 'Введите пароль'
+                  }
                 })}
                 type='password'
                 id='password'
